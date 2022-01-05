b8b80fc42d3965ad13381e3087d7146a9257aa33
rj1337 <mathesonroland@gmail.com>
2022-01-05 15:36:06 +0900

    add profile route to app.js and setup mongoDB cluster

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -21,51 +21,51 @@ const asyncHandler = require("express-async-handler");
 
 exports.searchProfiles = asyncHandler(async (req, res, next) => {
 
-   const profiles = await Profile.find ({});
-  
-   res.status(200).json({ profiles: profiles });
+  const profiles = await Profile.find({});
+
+  res.status(200).json({ profiles: profiles });
 });
 
 
 exports.searchProfileById = asyncHandler(async (req, res, next) => {
 
-    const profile = await Profile.findById(req.params.id).populate('user') //populate with reviews, messages and payment methods once those features are added
-   // we don't want to show all props of the profile to anyone who searches thier ID so this will change based on the inputs
-    if (!profile) {
-      res.status(404);
-      throw new Error("No profile found in search");
-    }
-  
-    res.status(200).json({ profile: profile });
- });
+  const profile = await Profile.findById(req.params.id).populate('user') //populate with reviews, messages and payment methods once those features are added
+  // we don't want to show all props of the profile to anyone who searches thier ID so this will change based on the inputs
+  if (!profile) {
+    res.status(404);
+    throw new Error("No profile found in search");
+  }
+
+  res.status(200).json({ profile: profile });
+});
 
 exports.updateProfile = asyncHandler(async (req, res, next) => {
 
-    try {
+  try {
 
-        const profile = await Profile.findByIdUpdate(req.params.id, {...req.body.profile}) //update props based on what was sent in the req body
-    
-        await profile.save();
-    
-        res.status(200).json({ profile: profile });
-      } catch (e) {
-        res.status(404).json({ message: e });
-      }
+    const profile = await Profile.findByIdUpdate(req.params.id, { ...req.body.profile }) //update props based on what was sent in the req body
 
- });
+    await profile.save();
+
+    res.status(200).json({ profile: profile });
+  } catch (e) {
+    res.status(404).json({ message: e });
+  }
+
+});
 
 
 exports.createProfile = asyncHandler(async (req, res, next) => {
 
-    try {
+  try {
 
-        const profile = new Profile(req.body.profile) 
-    
-        await profile.save();
-    
-        res.status(200).json({ profile: profile });
-      } catch (e) {
-        res.status(404).json({ message: e });
-      }
+    const profile = new Profile(req.body.profile)
 
- });
+    await profile.save();
+
+    res.status(200).json({ profile: profile });
+  } catch (e) {
+    res.status(404).json({ message: e });
+  }
+
+});
