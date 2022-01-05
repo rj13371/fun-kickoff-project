0191099ddce9e26620d1dc0e9125ec6a1d13f914
rj1337 <mathesonroland@gmail.com>
2022-01-05 17:35:56 +0900

    format with prettier and refactor readme

--- server/README.md
+++ server/README.md
@@ -4,6 +4,7 @@
 2. Run `npm install` to install packages
 3. Create your environment variable (.env) file (and add your MONGO_URI to the .env vars)
 4. Run `npm run dev` to start the server
+5. Open a new terminal window and cd back into the `server` file
 5. Run `npm run test` to run the tests
 6. Done!
 ---
--- server/test/ping.spec.js
+++ server/test/ping.spec.js
@@ -1,26 +1,26 @@
-var expect  = require('chai').expect;
+var expect = require('chai').expect;
 var request = require('request');
 
 
-describe("profile GET route", function() {
+describe("profile GET route", function () {
 
   var url = "http://localhost:3001/profiles/profiles"
 
-  it("returns status 200", function() {
-    request(url, function(error, response, body) {
+  it("returns status 200", function () {
+    request(url, function (error, response, body) {
       expect(response.statusCode).to.equal(200);
     });
   });
 
 });
 
-describe("ping for logout post request", function() {
+describe("ping for logout post request", function () {
 
   var url = "http://localhost:3001/auth/logout/"
 
-  it("returns status 200", function() {
-    request.post(url, function(error, response, body) {
+  it("returns status 200", function () {
+    request.post(url, function (error, response, body) {
       expect(response.statusCode).to.equal(200);
     });
-  });        
+  });
 });
