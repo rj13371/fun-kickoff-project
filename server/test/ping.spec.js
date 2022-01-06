5329d575e0f94781b657a152876e8cc7ae5c8361
rj1337 <mathesonroland@gmail.com>
2022-01-06 12:45:16 +0900

    made requested changes , changed var to const for consistency

--- server/test/ping.spec.js
+++ server/test/ping.spec.js
@@ -1,10 +1,10 @@
-var expect = require('chai').expect;
-var request = require('request');
+const expect = require('chai').expect;
+const request = require('request');
 
 
 describe("profile GET route", function () {
 
-  var url = "http://localhost:3001/profiles/profiles"
+  const url = "http://localhost:3001/profiles/profiles"
 
   it("returns status 200", function () {
     request(url, function (error, response, body) {
@@ -16,7 +16,7 @@ describe("profile GET route", function () {
 
 describe("ping for logout post request", function () {
 
-  var url = "http://localhost:3001/auth/logout/"
+  const url = "http://localhost:3001/auth/logout/"
 
   it("returns status 200", function () {
     request.post(url, function (error, response, body) {
