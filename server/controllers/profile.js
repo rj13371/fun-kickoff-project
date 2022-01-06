8e41d933c2e84e030dba988ca5e8481ce6feda3e
rj1337 <mathesonroland@gmail.com>
2022-01-06 12:43:04 +0900

    made requested changes, changed status codes, fixed findbyidandupdate, and changed the function name to a more applicable name

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -1,55 +1,37 @@
-// We would like to create the following controllers for creating, updating or getting profiles. Include the routes for this ticket as well
-
-// - CREATE
-
-// - Given parameters passed in, create a profile.
-
-// - UPDATE
-
-// - Given an ID and new parameters, update the profile
-
-// - GET
-
-// - Given an ID, return profile with that ID
-
-// - GET all
-
-// - A list of profiles
 const Profile = require("../models/Profile");
 const asyncHandler = require("express-async-handler");
 
-
-exports.searchProfiles = asyncHandler(async (req, res, next) => {
+exports.returnAllProfiles = asyncHandler(async (req, res, next) => {
 
   const profiles = await Profile.find({});
 
-  res.status(200).json({ profiles: profiles });
+  res.status(200).json({ profiles });
 });
 
 
 exports.searchProfileById = asyncHandler(async (req, res, next) => {
 
-  const profile = await Profile.findById(req.params.id).populate('user') //populate with reviews, messages and payment methods once those features are added
-  // we don't want to show all props of the profile to anyone who searches thier ID so this will change based on the inputs
+  const profile = await Profile.findById(req.params.id).populate('user') 
+
   if (!profile) {
     res.status(404);
     throw new Error("No profile found in search");
   }
 
-  res.status(200).json({ profile: profile });
+  res.status(200).json({ profile });
 });
 
 exports.updateProfile = asyncHandler(async (req, res, next) => {
 
   try {
 
-    const profile = await Profile.findByIdUpdate(req.params.id, { ...req.body.profile }) //update props based on what was sent in the req body
+    const profile = await Profile.findByIdAndUpdate(req.params.id, { ...req.body.profile })
 
     await profile.save();
 
-    res.status(200).json({ profile: profile });
+    res.status(200).json({ profile });
   } catch (e) {
-    res.status(404).json({ message: e });
+    res.status(400).json({ message: e });
   }
 
 });
@@ -63,9 +45,9 @@ exports.createProfile = asyncHandler(async (req, res, next) => {
 
     await profile.save();
 
-    res.status(200).json({ profile: profile });
+    res.status(200).json({ profile });
   } catch (e) {
-    res.status(404).json({ message: e });
+    res.status(400).json({ message: e });
   }
 
 });
--- server/routes/profile.js
+++ server/routes/profile.js
@@ -8,10 +8,7 @@ router.route("/profiles").get(searchProfiles);
 
 router.route("/create_profile").post(protect, createProfile);
 
-router.route("/:id").get(searchProfileById); // im not sure if we are using this route to display profiles to a user once they are logged
-//in and see their own profile, or just to see other users profiles, its not clear to me, so if we want to display
-//all props of the profile to any user when they search, then we obviously do not want to send sensitive details of the persons profile
-//we can add protect middleware if we only want to show profile details to the logged in user, but for now there is no protect auth
+router.route("/:id").get(searchProfileById);
 
 router.route("/:id/update_profile").put(protect, updateProfile);
 
