af6d25171f18d7eef6c5118d3f98c5e061ac6dee
rj1337 <mathesonroland@gmail.com>
2022-01-07 14:41:53 +0900

    add additional tests

--- server/test/auth.spec.js
+++ server/test/auth.spec.js
@@ -1,7 +1,5 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const mongoose = require("mongoose");
-const User = require ('../models/User')
 let expect = chai.expect;
 let app = 'localhost:3001';
 
@@ -9,10 +7,10 @@ chai.use(chaiHttp);
 
 describe("Auth API", function () {
 
-    const testEmail = `tester${Math.random().toFixed(3)}@gmail.com`
+    const testEmail = `tester${Math.random().toFixed(3)}@gmail.com` // Generate random fake email to register new user
+    const agent = chai.request.agent(app);
 
-    it("Run request", async () => {
-      const agent = chai.request.agent(app);
+    it("creates a new user and returns a JWT token", async () => {
       const res = await agent.post('/auth/register').send({
         'username': `testuser${Math.random().toFixed(3)}`,
         'email': `${testEmail}`,
@@ -20,5 +18,44 @@ describe("Auth API", function () {
     });
       expect(agent).to.have.cookie('token');
     });
+
+    it("logins successfully with token and email and pass", async () => {
+        const res = await agent.post('/auth/login')
+        .send({
+            'email': `${testEmail}`,
+            'password': 'tester'
+        })
+        expect(agent).to.have.cookie('token');
+        expect(res).to.have.status(200)
+      });
+
+      it("loads user successfully", async () => {
+        const res = await agent.post('/auth/user')
+        expect(res).to.have.status(200)
+      });
+
+      it("logout removes the auth token", async () => {
+        const res = await agent.post('/auth/logout')
+        expect(agent).to.not.have.cookie('token');
+      });
+
+      it("cannot register with already existing email", async () => {
+        const res = await agent.post('/auth/register').send({
+          'username': `testuser${Math.random().toFixed(3)}`,
+          'email': `roland@gmail.com`,
+          'password': 'tester'
+      });
+        expect(agent).to.not.have.cookie('token');
+      });
+
+      it("fails login with incorrect password", async () => {
+        const res = await agent.post('/auth/login')
+        .send({
+            'email': `${testEmail}`,
+            'password': 'badpassword'
+        })
+        expect(res).to.have.status(401)
+      });
+
   });
 
